139e3793334a1edef000c105df51f1beddc32cc3
MaxOzymok <ozymokmax1@ukr.net>
2020-06-14 21:52:57 +0300

    Updates

---
+++ precache-manifest.5b46899edfbc3eabcf9fb65c141a3314.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5cc297c54db59bf4e1b802e86bab8ed",
+    "url": "/RegistrationForm/index.html"
+  },
+  {
+    "revision": "501aead135404b6a3a5c",
+    "url": "/RegistrationForm/static/css/main.409f8098.chunk.css"
+  },
+  {
+    "revision": "f29aa4cc9e7aec55874e",
+    "url": "/RegistrationForm/static/js/2.b08b974f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RegistrationForm/static/js/2.b08b974f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "501aead135404b6a3a5c",
+    "url": "/RegistrationForm/static/js/main.9029bdf0.chunk.js"
+  },
+  {
+    "revision": "5ee240d5ad7cf726d9fd",
+    "url": "/RegistrationForm/static/js/runtime-main.142b60ec.js"
+  }
+]);
